548764ff6e2a3d746a2e240792ed683832803c3a
vivek-437 <vivekgupta2742203@gmail.com>
2025-01-05 00:14:43 +0530

    done template integration

---
+++ resources/views/layouts/head-css.blade.php
@@ -0,0 +1,11 @@
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/vendors/feather/feather.css')}}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/vendors/css/vendor.bundle.base.css')}}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/vendors/ti-icons/css/themify-icons.css')}}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/vertical-layout-light/style.css')}}">
+  <!-- endinject -->
---
+++ resources/views/layouts/vendor-script.blade.php
@@ -0,0 +1,15 @@
+ <!-- plugins:js -->
+ <script src="{{ URL::asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+ <!-- endinject -->
+ <!-- Plugin js for this page -->
+ <script src="{{ URL::asset('assets/js/dataTables.select.min.js')}}"></script>
+
+ <!-- End plugin js for this page -->
+ <!-- inject:js -->
+ <script src="{{ URL::asset('assets/js/off-canvas.js')}}"></script>
+ <script src="{{ URL::asset('assets/js/hoverable-collapse.js')}}"></script>
+ <script src="{{ URL::asset('assets/js/template.js')}}"></script>
+ <script src="{{ URL::asset('assets/js/settings.js')}}"></script>
+ <!-- endinject -->
+ <!-- Custom js for this page-->
+ <!-- End custom js for this page-->
